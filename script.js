9df9b0f4137d35fd38a055d63ca34cda1ea6b171
Anja Wurlitzer <anja.wurlitzer.coding@gmail.com>
2023-01-18 12:08:10 +0100

    feat: first solution

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 const fast = document.querySelector("#fast");
 const cheap = document.querySelector("#cheap");
 const good = document.querySelector("#good");
+let last = null;
 
-function checkbox(a, b, c) {
+function checkbox1() {
   if (fast.checked && cheap.checked && good.checked) {
-    good.removeAttribute("checked");
+    last.checked = false;
   }
+  last = fast;
 }
 
-fast.addEventListener("change", function () {
-  checkbox;
-});
-cheap.addEventListener("change", function () {
-  checkbox;
-});
-good.addEventListener("change", function () {
-  checkbox;
-});
+function checkbox2() {
+  if (fast.checked && cheap.checked && good.checked) {
+    last.checked = false;
+  }
+  last = cheap;
+}
+
+function checkbox3() {
+  if (fast.checked && cheap.checked && good.checked) {
+    last.checked = false;
+  }
+  last = good;
+}
+
+fast.addEventListener("change", checkbox1);
+cheap.addEventListener("change", checkbox2);
+good.addEventListener("change", checkbox3);
 
 console.log(fast.checked);
 
 //Funktion, abfragt, ob alle gechecked sind
 //eventListener, die die funktion aufruft
-//getrandomnit
+//was wurde zu letzt gechecked?
